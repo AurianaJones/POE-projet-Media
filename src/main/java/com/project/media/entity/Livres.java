45034bd7b70f084f34d89f048f8e6bf7c7e2b479
Auriana Jones <aurianajones@outlook.fr>
2022-10-07 14:46:00 +0200

    Entité utilisateur & livre

--- src/main/java/com/project/media/entity/Livres.java
+++ src/main/java/com/project/media/entity/Livres.java
@@ -1,5 +1,44 @@
 package com.project.media.entity;
 
-public class Livres {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
+@Entity
+public class Livres extends Items{
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "taskSequenceGenerator")
+    @SequenceGenerator(name = "taskSequenceGenerator")
+	private Long id;
+	@Column (name = "auteur")
+	private String auteur;
+	@Column (name = "numero_ISBN")
+	private String isbnNum;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAuteur() {
+		return auteur;
+	}
+
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+
+	public String getIsbnNum() {
+		return isbnNum;
+	}
+
+	public void setIsbnNum(String isbnNum) {
+		this.isbnNum = isbnNum;
+	}
 }
--- src/main/java/com/project/media/entity/Utilisateurs.java
+++ src/main/java/com/project/media/entity/Utilisateurs.java
@@ -1,5 +1,49 @@
 package com.project.media.entity;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
 public class Utilisateurs {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "taskSequenceGenerator")
+    @SequenceGenerator(name = "taskSequenceGenerator")
+	private Long Id;
+	@Column
+	private String email;
+	@Column
+	private String nom;
+	@Column
+	private String prenom;
+	
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
 }
