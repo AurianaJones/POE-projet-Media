92ccc0946123734a35e10d5147313f93e3abc38d
Auriana Jones <aurianajones@outlook.fr>
2022-10-07 19:55:08 +0200

    ajout de l'entité Emprunt et renommage d''Items' en 'Objets'

---
+++ src/main/java/com/project/media/entity/Emprunts.java
@@ -0,0 +1,64 @@
+package com.project.media.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "emprunts")
+public class Emprunts {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "date_d_emprunt")
+	private LocalDate dateEmprunt;
+
+	@Column(name = "date_de_retour")
+	private LocalDate dateRetour;
+
+	@ManyToOne
+	private Utilisateurs utilisateur;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDate getDateEmprunt() {
+		return dateEmprunt;
+	}
+
+	public void setDateEmprunt(LocalDate dateEmprunt) {
+		this.dateEmprunt = dateEmprunt;
+	}
+
+	public LocalDate getDateRetour() {
+		return dateRetour;
+	}
+
+	public void setDateRetour(LocalDate dateRetour) {
+		this.dateRetour = dateRetour;
+	}
+
+	public Utilisateurs getUtilisateur() {
+		return utilisateur;
+	}
+
+	public void setUtilisateur(Utilisateurs utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+}
