5968ac864f270766de3cae9355fa175df694d014
Auriana Jones <aurianajones@outlook.fr>
2022-10-21 11:33:32 +0200

    Trouver tout les objets par caractéristique

--- src/main/java/com/project/media/repository/ObjetsRepository.java
+++ src/main/java/com/project/media/repository/ObjetsRepository.java
@@ -5,10 +5,17 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.project.media.entity.Cds;
+import com.project.media.entity.Dvds;
+import com.project.media.entity.Livres;
 import com.project.media.entity.Objets;
 
 public interface ObjetsRepository extends JpaRepository<Objets, Long> {
 	List<Objets> findAllByQuantite(int quantite);
 	List<Objets> findAllByDate (LocalDate date_parution);
 	
+	//Trouver les objet par type 
+	List<Cds> findAllCdById(int id);
+	List<Dvds> findAllDvdById(int id);
+	List<Livres> findAllLivreById(int id);
 }
