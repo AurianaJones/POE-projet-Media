c982b5fc11bfb452cf579ffbb66e10177e1f719c
Auriana Jones <aurianajones@outlook.fr>
2022-10-21 10:06:12 +0200

    fichier inutile surprimer

--- src/main/java/com/project/media/repository/DvdsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.media.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.media.entity.Dvds;
-
-public interface DvdsRepository extends JpaRepository<Dvds, Long> {
-
-}
