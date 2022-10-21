544533a702336d5a5991fd8f8bfda0131125b8bc
Auriana Jones <aurianajones@outlook.fr>
2022-10-21 11:34:17 +0200

    Création du Test de ObjetRepository

---
+++ src/test/java/com/project/media/test/repository/ObjetRepositoryTest.java
@@ -0,0 +1,13 @@
+package com.project.media.test.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.project.media.repository.ObjetsRepository;
+
+public class ObjetRepositoryTest {
+	
+	@Autowired
+	private ObjetsRepository objetRepository;
+	
+
+}
